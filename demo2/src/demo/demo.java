7937191726dec8834b8e01e6b75500c851bfc178
momo920206 <469379893@qq.com>
2016-10-28 10:43:38 +0800

    School First
    Signed-off-by: momo920206 <469379893@qq.com>

---
+++ demo2/src/demo/demo.java
@@ -0,0 +1,16 @@
+package demo;
+import Sharp.*;
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		Triangle.CreatIsoscelesTriangle1(10);
+		System.out.println();
+		Triangle.CreatIsoscelesTriangle2(10);
+		System.out.println();
+		Triangle.CreatIsoscelesTriangle3(10);
+		System.out.println();
+		Triangle.CreatIsoscelesTriangle4(10);
+	}
+
+}
